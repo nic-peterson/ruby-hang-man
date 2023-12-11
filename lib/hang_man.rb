e8e637105d13bf9bf46f85ad8c031e145fa63e9c
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-12-10 17:28:19 -0800

    Finished Word class.

--- lib/hang_man.rb
+++ lib/hang_man.rb
@@ -1,1 +1,25 @@
-puts "Hello Hang Man!"
+class Word
+  FILE_PATH = "words.txt"
+  attr_reader :word
+
+  def initialize
+    @words = generate_words_array
+    @word = @words[rand(0..@words.length)]
+  end
+
+  def generate_words_array
+    lines = File.readlines(FILE_PATH)
+    words = []
+    lines.each do |line|
+      word = line.chomp
+      if (word.length > 4 && word.length < 13)
+        words.push(word)
+      end
+    end
+    return words
+  end
+end
+
+word = Word.new
+
+puts word.word
