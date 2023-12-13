39e8046a83ba05f9ca6095cfb60523ae6fc320ac
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-12-12 18:40:15 -0800

    Completed Display class.

--- lib/hang_man.rb
+++ lib/hang_man.rb
@@ -22,8 +22,38 @@ class Word
   end
 end
 
+module Display
+  # Displays the current state of the game
+  def show_current_state(current_state, remaining_attempts, guessed_letters)
+    puts "Remaining attempts: #{remaining_attempts}"
+    puts "Current word: #{current_state}"
+    puts "Guessed letters: #{guessed_letters.join(" ")}"
+  end
+
+  # Displays a message when the player makes a correct guess
+  def show_correct_guess_message(guess)
+    puts "\e[32m#{guess} is correct!\e[32m\e[0m"
+  end
+
+  # Displays a message when the player makes an incorrect guess
+  def show_incorrect_guess_message(guess)
+    puts "\e[31m#{guess} is not correct\e[31m\e[0m"
+  end
+
+  # Displays a winning message when the player guesses the word
+  def show_win_message(word)
+    puts "e[32m\eCongratulations! You've guessed the word! #{word}e[32m\e[0m"
+  end
+
+  # Displays a losing message when the player runs out of attempts
+  def show_lose_message(word)
+    puts "\e[36mGame over! The secret word was: #{word}\e[36m\e[0m"
+  end
+end
+
 class Game
   NUMBER_OF_ATTEMPTS = 6
+  include Display
 
   def initialize
     @word = Word.new.word
@@ -32,32 +62,36 @@ class Game
     @guess_array = []
   end
 
-  def display_all
-    puts @word
-    puts @current_state
-    puts @remaining_attempts
-  end
+  # def display_all
+  # puts @word
+  # puts @current_state
+  # puts @remaining_attempts
+  # end
 
-  def display
-    puts "Remaining attempts: #{@remaining_attempts}"
-    puts "#{@current_state}"
-    puts "Guesses: #{@guess_array.join(" ")}"
-  end
+  # def display
+  # puts "Remaining attempts: #{@remaining_attempts}"
+  # puts "#{@current_state}"
+  # puts "Guesses: #{@guess_array.join(" ")}"
+  # end
 
   def play
     until (@remaining_attempts == 0)
-      display
+      # display
+      puts @word
+      show_current_state(@current_state, @remaining_attempts, @guess_array)
       guess = prompt_guess
 
       if @current_state == @word
-        puts "Congratulations! You've guessed the word!"
+        show_win_message(@word)
+        # puts "Congratulations! You've guessed the word!"
         return
       else
         provide_feedback(guess)
       end
     end
 
-    puts "\e[36mGame over! The secret word was: #{@word}\e[36m\e[0m"
+    show_lose_message(@word)
+    # puts "\e[36mGame over! The secret word was: #{@word}\e[36m\e[0m"
   end
 
   private
@@ -73,17 +107,19 @@ class Game
   end
 
   def valid_guess?(guess)
-    guess.length == 1 && guess.match?(/\A[a-zA-Z]+\z/) && !@current_state.include?(guess) && !@guess_array.include?(guess)
+    guess.length == 1 && guess.match?(/\A[a-zA-Z]+\z/) &&
+      !@current_state.include?(guess) && !@guess_array.include?(guess)
   end
 
   def provide_feedback(guess)
     if @word.include?(guess)
-      puts "\e[32m#{guess} is correct!\e[32m\e[0m"
+      show_correct_guess_message(guess)
+      # puts "\e[32m#{guess} is correct!\e[32m\e[0m"
       matching_indices = find_matching_indices(guess)
       update_current_state(guess, matching_indices)
 
     else
-      puts "\e[31m#{guess} is *not* correct\e[31m\e[0m"
+      show_incorrect_guess_message(guess)
       @remaining_attempts -= 1
     end
     @guess_array.push(guess)
